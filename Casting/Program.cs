5a562400e0be1949153402dfc06577f7b60089de
Jalə Quliyeva <quliyevajale52@gmail.com>
2022-03-24 22:23:27 +0400

    file added

---
+++ Casting/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Casting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Celsius celsius = new Celsius(2);
+            //Kelvin kelvin = celsius;
+            Console.WriteLine(celsius.Degree);
+        }
+    }
+    enum Currency
+    {
+
+        [Display(Name = " Dollar $")]
+        USD,
+        [Display(Name = "Yevro $")]
+        EUR,
+        [Display(Name = "Turk lirasi ¥")]
+        TRY
+    }
+    public void ExChange(Enum pul, double azn)
+    {
+        
+    }
+
+
+    class Kelvin
+    {
+        public int Degree { get; set; }
+        public Kelvin(int degree)
+        {
+            Degree = degree;    
+        }
+
+        //public static implicit operator Kelvin(Celsius v)
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+    class Celsius
+    {
+        public int Degree { get; set; }
+        public Celsius(int degree)
+        {
+            Degree = degree;
+        }
+        public static implicit operator Celsius(Kelvin k)
+        {
+            return new Celsius(k.Degree/273);   
+        }
+
+    }
+}
